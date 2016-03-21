069307b80e6e8cb09917d5db7ab59a382b915059
Markz2z <ustczmk@gmail.com>
2016-03-21 20:23:03 +0800

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+A basic linear regression algorithm implemented in C++.
---
+++ linear_regression.cpp
@@ -0,0 +1,13 @@
+
+
+//simple y = ax + b model
+//(x, y) represent the input data
+int* linear(int x[], int len_x, int y[], int len_y) {
+	int result[2];
+	
+	return result;
+}
+
+int main() {
+	return 1;
+}
