f090a1667d5e6238f1e3e0d33976d9b626ba5545
Markz2z <ustczmk@gmail.com>
2018-12-17 18:55:02 +0800

    reconsturct, pass compile

--- BPNeuralNetwork/NeuralNetworkBP.cpp
+++ BPNeuralNetwork/NeuralNetworkBP.cpp
@@ -2,54 +2,44 @@
 // Created by ustczmk on 17/2/14.
 //
 #include "NeuralNetworkBP.h"
+#include <sstream>
 
 NeuralNetwork::NeuralNetwork(int input_nodes, const std::vector<int>& hidden_layer_param,
-        int output_nodes) {
+        int output_nodes) : _input_layer(input_nodes), _output_layer(output_nodes),
+        _hidden_layers(hidden_layer_param.size()), _loss(100.f) {
     srand((unsigned)time(NULL));
-    error = 100.f;
-
     int hidden_layers = hidden_layer_param.size();
-
-    _output_layer.resize(output_nodes);
-
     //init input layer
-    _input_layer.resize(input_nodes);
     for (auto& node : _input_layer) {
-        int first_hidden_nodes = hidden_layer_param[0].size();
-        node->weight.resize(first_hidden_nodes);
-        for (int j = 0; j < node->weight.size(); ++j) {
-            node->weight[j] = genRandom();
+        int first_hidden_nodes = _hidden_layers[0].size();
+        node.weight.resize(first_hidden_nodes);
+        node.weight_derivative_sum.resize(first_hidden_nodes, 0.f);
+        for (int j = 0; j < first_hidden_nodes; ++j) {
+            node.weight[j] = genRandom();
         }
-        node->weight_derivative_sum.resize(first_hidden_nodes, 0.f);
     }
 
-    //init hide layer
-    _hidden_layers.resize(hidden_layers);
+    //init hidden layer
     int hidden_layer_idx = 0;
     for (auto& hidden_layer : _hidden_layers) {
         hidden_layer.resize(hidden_layer_param[hidden_layer_idx]);
         for (auto& hidden_node : hidden_layer) {
-            int weight_cnt = hidden_layer_idx == hidden_layers - 1 ?
-                output_nodes : hidden_layer_param[hidden_layer_idx + 1].size();
-            hidden_node.weight.resize(weight_cnt);
-            for (int k = 0; k < weight_cnt; ++k) {
+            int next_layer_nodes = hidden_layer_idx == hidden_layers - 1 ?
+                output_nodes : hidden_layer_param[hidden_layer_idx + 1];
+            hidden_node.weight.resize(next_layer_nodes);
+            for (int k = 0; k < next_layer_nodes; ++k) {
                 hidden_node.weight[k] = genRandom();
             }
-            int derivatives = i == 0 ? input_nodes : hidden_layer_param[hidden_layer_idx - 1].size();
-            hidden_node.weight_derivative_sum.resize(derivatives, 0.f);
-            hidden_node.bias = genRandom();
+            int prev_layer_nodes = hidden_layer_idx == 0 ? input_nodes : hidden_layer_param[hidden_layer_idx - 1];
+            hidden_node.weight_derivative_sum.resize(prev_layer_nodes, 0.f);
         }
         ++hidden_layer_idx;
     }
-
-    //init output layer
-    _output_layer.resize(output_nodes);
 }
 
-void NeuralNetwork::forwardPropagation() {
-
+void NeuralNetwork::feed_forward() {
     // hidden layer
-    for (int layer_idx = 0: layer_idx < _hidden_layers.size(); ++layer_idx) {
+    for (int layer_idx = 0; layer_idx < _hidden_layers.size(); ++layer_idx) {
         auto& layer = _hidden_layers[layer_idx];
         for (int node_idx = 0; node_idx < layer.size(); ++node_idx) {
             auto& node = layer[node_idx];
@@ -59,7 +49,7 @@ void NeuralNetwork::forwardPropagation() {
                     sum += input_node.value * input_node.weight[node_idx];
                 }
             } else {
-                auto& prev_layer = _hidden_layers[layer_idx - 1];
+                const auto& prev_layer = _hidden_layers[layer_idx - 1];
                 for (const auto& prev_node : prev_layer) {
                     sum += prev_node.value * prev_node.weight[node_idx];
                 }
@@ -68,93 +58,89 @@ void NeuralNetwork::forwardPropagation() {
         }
     }
 
+    // output layer
     for (int node_idx = 0; node_idx < _output_layer.size(); ++node_idx) {
         auto& output_node = _output_layer[node_idx];
         double sum = output_node.bias;
-        auto& hide_layer = _hidden_layers.back();
+        const auto& hide_layer = _hidden_layers.back();
         for (const auto& hide_node : hide_layer) {
             sum += hide_node.value * hide_node.weight[node_idx];
         }
-        output_node.compute_val = sigmoid(sum);
+        output_node.value = sigmoid(sum);
     }
 }
 
-void NeuralNetwork::backPropagation() {
-    for (int i = 0; i < OUTPUT_LAYER_NODES; ++i) {
-        double t = output_node[i]->compute_val - output_node[i]->real_val;
-        error += t * t / 2;
-        output_node[i]->derivative = t * output_node[i]->compute_val * (1 - output_node[i]->compute_val);
+void NeuralNetwork::back_propagation() {
+    for (auto& output_node : _output_layer) {
+        double delta = output_node.value - output_node.real_val;
+        output_node.derivative = delta * output_node.value * (1 - output_node.value);
+        _loss += delta * delta / 2;
     }
 
-    for (int i = HIDE_LAYERS - 1; i >= 0; --i) {
-        for (int j = 0; j < HIDE_LAYER_NODES; ++j) {
-            double sigmoid_derivative = hide_node[i][j]->value * (1 - hide_node[i][j]->value), sum = 0.f;
-            if (i==HIDE_LAYERS - 1) {
-                for (int k = 0; k < OUTPUT_LAYER_NODES; ++k) {
-                    sum += sigmoid_derivative * output_node[k]->derivative * hide_node[i][j]->weight[k];
-                }
-            } else {
-                for (int k = 0; k < HIDE_LAYER_NODES; ++k) {
-                    sum += sigmoid_derivative * hide_node[i+1][k]->derivative * hide_node[i][j]->weight[k];
-                }
+    for (auto layer = _hidden_layers.rbegin(); layer != _hidden_layers.rend(); ++layer) {
+        for (auto& node : *layer) {
+            double sigmoid_derivative = node.value * (1 - node.value), sum = 0.f;
+            auto next_layer = layer == _hidden_layers.rbegin() ? _output_layer : *(layer - 1);
+            int next_layer_idx = 0;
+            for (const auto& next_node : next_layer) {
+                sum += sigmoid_derivative * next_node.derivative * node.weight[next_layer_idx++];
             }
-            hide_node[i][j]->derivative = sum;
+            node.derivative = sum;
         }
     }
 
-    for (int i = 0; i < INPUT_LAYER_NODES; ++i) {
-        for (int j = 0; j < HIDE_LAYER_NODES; ++j) {
-            input_node[i]->weight_derivative_sum[j] += hide_node[0][j]->derivative * input_node[i]->value;
+    const auto& first_hidden_layer = _hidden_layers[0];
+    for (auto& node : _input_layer) {
+        for (int j = 0; j < first_hidden_layer.size(); ++j) {
+            node.weight_derivative_sum[j] += first_hidden_layer[j].derivative * node.value;
         }
     }
 
-    for (int i = 0; i < HIDE_LAYERS; ++i) {
-        for (int j = 0; j < HIDE_LAYER_NODES; ++j) {
-            hide_node[i][j]->bias_derivative_sum += hide_node[i][j]->derivative;
-            if (i==HIDE_LAYERS-1) {
-                for (int k = 0; k < OUTPUT_LAYER_NODES; ++k) {
-                    hide_node[i][j]->weight_derivative_sum[k] += output_node[k]->derivative * hide_node[i][j]->value;
-                }
-            } else {
-                for (int k = 0; k < HIDE_LAYER_NODES; ++k) {
-                    hide_node[i][j]->weight_derivative_sum[k] += hide_node[i+1][k]->derivative * hide_node[i][j]->value;
-                }
+    for (int layer_idx = 0; layer_idx < _hidden_layers.size(); ++layer_idx) {
+        auto& layer = _hidden_layers[layer_idx];
+        for (auto& node : layer) {
+            node.bias_derivative_sum += node.derivative;
+            const auto& next_layer = layer_idx == _hidden_layers.size() - 1 ? _output_layer : _hidden_layers[layer_idx + 1];
+            int next_idx = 0;
+            for (const auto& next_node : next_layer) {
+                node.weight_derivative_sum[next_idx++] += next_node.derivative * node.value;
             }
         }
     }
 
-    for (int i = 0; i < OUTPUT_LAYER_NODES; ++i) {
-        output_node[i]->bias_derivative_sum += output_node[i]->derivative;
+    for (auto& output_node : _output_layer) {
+        output_node.bias_derivative_sum += output_node.derivative;
     }
 }
 
 void NeuralNetwork::predict(DataGroup& test_input) {
-    for ( int i = 0; i < INPUT_LAYER_NODES; ++i) {
-        cout << test_input.in[i] << " ";
+    std::stringstream ss;
+    for (int i = 0; i < _input_layer.size(); ++i) {
+        ss << test_input.in[i] << "\n";
     }
-    setInputOutput(test_input);
-    forwardPropagation();
-    cout << " ====> ";
-    for (int i = 0; i < OUTPUT_LAYER_NODES; ++i) {
-        test_input.out[i] = output_node[i]->compute_val;
-        cout << output_node[i]->compute_val << " ";
+    initialize(test_input);
+    feed_forward();
+    ss << " ====== \n";
+    for (int i = 0; i < _output_layer.size(); ++i) {
+        test_input.out[i] = _output_layer[i].value;
+        ss << _output_layer[i].value << "\n";
     }
-    cout << endl;
+    std::cout << ss.str() << std::endl;
 }
 
-void NeuralNetwork::setInputOutput(DataGroup& data) {
-    for (int i = 0; i < INPUT_LAYER_NODES; ++i) {
-        input_node[i]->value = data.in[i];
+void NeuralNetwork::initialize(const DataGroup& data) {
+    for (int i = 0; i < _input_layer.size(); ++i) {
+        _input_layer[i].value = data.in[i];
     }
-    for (int i = 0; i < OUTPUT_LAYER_NODES; ++i) {
-        output_node[i]->real_val = data.out[i];
+    for (int i = 0; i < _output_layer.size(); ++i) {
+        _output_layer[i].real_val = data.out[i];
     }
 }
 
 /*
  * using for debug to dump all of the parameters in neural network
  * */
-void NeuralNetwork::printAllNode() {
+void NeuralNetwork::print_all_node() {
     std::stringstream ss;
     int idx = 0;
     for (const auto& node : _input_layer) {
@@ -174,65 +160,68 @@ void NeuralNetwork::printAllNode() {
 
     idx = 0;
     for (const auto& node : _output_layer) {
-        ss << "outputNode[" << idx++ << "] : " << node.compute_val << " ";
+        ss << "outputNode[" << idx++ << "] : " << node.value << " ";
     }
     ss << "\n";
     std::cout << ss.str() << std::endl;
 }
 
-void NeuralNetwork::train(vector<DataGroup>& train_set, double threshold) {
+void NeuralNetwork::train(const std::vector<DataGroup>& train_set, double threshold) {
     int layer, idx;
-    unsigned long train_size = train_set.size();
-    while(error > threshold) {
-        error = 0.f;
+    std::size_t train_size = train_set.size();
+    while(_loss > threshold) {
+        _loss = 0.f;
 
         //1. init
-        for (int i = 0; i < INPUT_LAYER_NODES; ++i) {
-            input_node[i]->weight_derivative_sum.assign(HIDE_LAYER_NODES, 0.f);
+        for (auto& input_node : _input_layer) {
+            input_node.weight_derivative_sum.assign(_hidden_layers[0].size(), 0.f);
         }
 
-        for (int i = 0; i < HIDE_LAYERS; ++i) {
-            unsigned long next_layer_nodes = i == HIDE_LAYERS - 1 ? OUTPUT_LAYER_NODES : HIDE_LAYER_NODES;
-            for (int j = 0; j < HIDE_LAYER_NODES; ++j) {
-                hide_node[i][j]->weight_derivative_sum.assign(next_layer_nodes, 0.f);
-                hide_node[i][j]->bias_derivative_sum = 0.f;
+        for (int i = 0; i < _hidden_layers.size(); ++i) {
+            auto& layer = _hidden_layers[i];
+            int next_layer_nodes = i == _hidden_layers.size() ? _output_layer.size() : _hidden_layers[i+1].size();
+            for (auto& node : layer) {
+                node.weight_derivative_sum.assign(next_layer_nodes, 0.f);
+                node.bias_derivative_sum = 0.f;
             }
         }
 
-        for (int i = 0; i < OUTPUT_LAYER_NODES; ++i) {
-            output_node[i]->bias_derivative_sum = 0.f;
+        for (auto& output_node : _output_layer) {
+            output_node.bias_derivative_sum = 0.f;
         }
 
         //2. train data
-        for (DataGroup& data : train_set) {
-            setInputOutput(data);
-            forwardPropagation();
-            backPropagation();
-            //printAllNode();
+        for (const auto& data : train_set) {
+            initialize(data);
+            feed_forward();
+            back_propagation();
+            //print_all_node();
         }
 
 
         //3. update weight and bias
-        for (int i = 0; i < INPUT_LAYER_NODES; ++i) {
-            for (int j = 0; j < HIDE_LAYER_NODES; ++j) {
-                input_node[i]->weight[j] -= LEARNING_RATE * input_node[i]->weight_derivative_sum[j] / train_size;
+        for (auto& input_node : _input_layer) {
+            for (int j = 0; j < _hidden_layers[0].size(); ++j) {
+                input_node.weight[j] -= LEARNING_RATE * input_node.weight_derivative_sum[j] / train_size;
             }
         }
 
-        for (layer = 0; layer < HIDE_LAYERS; ++layer) {
-            for (idx = 0; idx < HIDE_LAYER_NODES; ++idx) {
-                hide_node[layer][idx]->bias -= LEARNING_RATE * hide_node[layer][idx]->bias_derivative_sum / train_size;
-                for (int next_idx = 0; next_idx < HIDE_LAYER_NODES; ++next_idx) {
-                    hide_node[layer][idx]->weight[next_idx] -= LEARNING_RATE * hide_node[layer][idx]->weight_derivative_sum[next_idx] / train_size;
+        for (int layer_idx = 0; layer_idx < _hidden_layers.size(); ++layer_idx) {
+            auto& layer = _hidden_layers[layer_idx];
+            for (auto& node : layer) {
+                node.bias -= LEARNING_RATE * node.bias_derivative_sum / train_size;
+                int next_layer_size = layer_idx == _hidden_layers.size() - 1 ? _output_layer.size() : _hidden_layers[layer_idx+1].size();
+                for (int next_idx = 0; next_idx < next_layer_size; ++next_idx) {
+                    node.weight[next_idx] -= LEARNING_RATE * node.weight_derivative_sum[next_idx] / train_size;
                 }
             }
         }
 
-        for (idx = 0; idx < OUTPUT_LAYER_NODES; ++idx) {
-            output_node[idx]->bias -= LEARNING_RATE * output_node[idx]->bias_derivative_sum / train_size;
+        for (auto& output_node : _output_layer) {
+            output_node.bias -= LEARNING_RATE * output_node.bias_derivative_sum / train_size;
         }
 
-        error /= train_size;
-        cout << "error:" << error << endl;
+        _loss /= train_size;
+        std::cout << "error : " << _loss << std::endl;
     }
 }
--- BPNeuralNetwork/NeuralNetworkBP.h
+++ BPNeuralNetwork/NeuralNetworkBP.h
@@ -7,54 +7,45 @@
 #include <cmath>
 #include <cstdlib>
 #include <iostream>
-using namespace std;
 
-#define INPUT_LAYER_NODES 2
-#define HIDE_LAYERS 5
-#define HIDE_LAYER_NODES 7
-#define OUTPUT_LAYER_NODES 1
 #define LEARNING_RATE 0.9
 
-inline double genRandom() {
+double genRandom() {
     return ((2.0 * (double)rand() / RAND_MAX) - 1);
 }
 
-inline double sigmoid(double x) {
-    return ans = 1 / (1 + exp(-x));
+double sigmoid(double x) {
+    return 1 / (1 + exp(-x));
 }
 
 typedef struct NeuronNode {
-    vector<double> weight, weight_derivative_sum;
-    double value, bias, derivative, bias_derivative_sum;
+    NeuronNode() : bias(genRandom()) {}
+    std::vector<double> weight, weight_derivative_sum;
+    double real_val, value, bias, derivative, bias_derivative_sum;
 } NeuronNode;
 
 typedef struct InputNode {
-    vector<double> weight, weight_derivative_sum;
+    std::vector<double> weight, weight_derivative_sum;
     double value;
 } InputNode;
 
-typedef struct OutputNode {
-    OutputNode() bias(genRandom()) {}
-    double compute_val, real_val, bias, derivative, bias_derivative_sum;
-} OutputNode;
-
 typedef struct DataGroup {
-    vector<double> in, out;
+    std::vector<double> in, out;
 } DataGroup;
 
 class NeuralNetwork {
 public:
-    NeuralNetwork(int input_nodes, int hiden_layers, int output_nodes);
-    void forwardPropagation();
-    void backPropagation();
-    void train(vector<DataGroup>& train_set, double threshold);
+    NeuralNetwork(int input_nodes, const std::vector<int>& hidden_layer_param, int output_nodes);
+    void feed_forward();
+    void back_propagation();
+    void train(const std::vector<DataGroup>& train_set, double threshold);
     void predict(DataGroup& test_input);
-    void printAllNode();
-    void setInputOutput(DataGroup& output);
+    void print_all_node();
+    void initialize(const DataGroup& output);
 
 private:
     std::vector<InputNode> _input_layer;
-    std::vecotr<vector<NeuronNode>> _hidden_layers;
-    std::vector<OutputNode> _output_layer;
-    double error;
+    std::vector<std::vector<NeuronNode>> _hidden_layers;
+    std::vector<NeuronNode> _output_layer;
+    double _loss;
 };
